6fa55bfd560c1218330827ad4e014b5adab06b15
Prashant Deva <prashant.deva@gmail.com>
2023-09-24 14:01:19 -0700

    more

--- mine.nix
+++ mine.nix
@@ -4,6 +4,12 @@ in
   pkgs.mkShell {
     packages = [
       pkgs.python3
+      pkgs.curl
     ];
   }
-
+  env = {
+      ENV="local"
+      # Database credentials
+      DB_USER = "db_user";
+      DB_PASSWORD = "super secret don't look";
+  };
