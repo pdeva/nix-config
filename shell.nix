153fe4c7caaef9722decd00e0313a8e5acb8772a
Prashant Deva <prashant.deva@gmail.com>
2023-09-24 14:04:57 -0700

    fix

--- shell.nix
+++ shell.nix
@@ -6,10 +6,10 @@ in
       pkgs.python3
       pkgs.curl
     ];
-  }
   env = {
-      ENV="local"
+      ENV="local";
       # Database credentials
       DB_USER = "db_user";
       DB_PASSWORD = "super secret don't look";
   };
+}
